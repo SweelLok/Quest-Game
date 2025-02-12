97afc3f528e38bd3003fe2a0da37747296fdd206
SweelLok <ukr.vadya@gmail.com>
2025-02-12 23:28:34 +0200

    Last commit

--- app/forms/feedback.py
+++ app/forms/feedback.py
@@ -3,6 +3,7 @@ from wtforms import TextAreaField, SelectField, SubmitField, validators
 
 
 class FeedbackForm(FlaskForm):
-    text = TextAreaField("Your feedback", validators=[validators.InputRequired()], render_kw={"placeholder": "Write your text here..."})
+    text = TextAreaField("Your feedback", validators=[validators.InputRequired()], 
+                         render_kw={"placeholder": "Write your text here..."})
     rating = SelectField("Rating")
     submit = SubmitField("Send")
