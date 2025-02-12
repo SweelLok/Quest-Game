7ae82d341642b52adda8e2d8d0db4131ec7bfc22
SweelLok <ukr.vadya@gmail.com>
2025-02-12 22:57:20 +0200

    Last version of project

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_compress import Compress
 
 from connection import create_tables
 
 
 app = Flask(__name__)
+Compress(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config["SECRET_KEY"] = "q"
 create_tables()
- 
